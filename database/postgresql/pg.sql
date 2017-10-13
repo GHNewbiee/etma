190f38b832e9d8453d4bf44fa90fce2104ce8c30
Georgios - Pavlos <GHNewbiee@users.noreply.github.com>
2017-10-13 16:57:55 +0200

    Create pg.sql

---
+++ database/postgresql/pg.sql
@@ -0,0 +1,23 @@
+-------------------------------------------------
+-- Filename:        pg.sql
+-- Version:         0.0.1
+-- Release Date:    01/01/2001
+-- Description:     ...
+-- Notes:           ...
+--
+-- Public License: CC-BY-NC-SA 4.0 International License
+--   http://creativecommons.org/licenses/by-nc-sa/4.0/
+--
+-- Commercial License Agreement: All commercial rights are reserved by ...
+--   https://github.com/GHNewbiee/etma/blob/master/COMMERCIAL_LICENSE_AGREEMENT.md
+--
+-- Author(s): ...
+-- Major contributor(s): ...
+--
+-- Αcknowledg(e)ment: DBeaver, pgAdmin, PostgreSQL, Visual Studio Code, & Contributors
+
+-- Bug report and new feature request:
+--   https://github.com/GHNewbiee/etma/blob/master/.github/CONTRIBUTING.md
+-- General or HowTo question:
+--   ...
+-------------------------------------------------
